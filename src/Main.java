11927a0b14b425d80a9d3623d582cfda194c10b5
viktor-parment <viktor.parment@student.chasacademy.se>
2026-08-24 09:23:24 +0200

    Second commit with an enhanced for-loop

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 void main () {
     String[] list = {"Viktor", "My", "Lilly", "Alexander", "Tony"};
     String longestName = list[0];
-    for (int i = 0; i < list.length ; i++){
-        if (list[i].length() > longestName.length())
-            longestName = list[i];
+    for (String namn : list) {
+        if (namn.length() > longestName.length()) {
+            longestName = namn;
         }
-    IO.println("Det längsta namnet är: " + longestName);
     }
+    IO.println("Det längsta namnet är: " + longestName);
+}
